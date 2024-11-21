28308066115d15d50fda6e92c42ed9a73d6713eb
Javier Sangrador <javier.sangrador@LDCXY309Y2F.local>
2024-11-21 01:46:00 +0100

    wip

--- src/test/java/com/javinsnc/microservices/orderservice/stubs/InventoryClientStub.java
+++ src/test/java/com/javinsnc/microservices/orderservice/stubs/InventoryClientStub.java
@@ -5,7 +5,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.*;
 public class InventoryClientStub {
 
     public static void stubInventoryCall(String skuCode, int quantity) {
-        stubFor(get(urlEqualTo("/api/inventory?skyCode=" + skuCode + "&quantity=" + quantity))
+        stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withHeader("Content-Type", "application/json")
