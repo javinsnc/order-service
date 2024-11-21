d3d1f4cdffae26d25113379bc3339e07358c02ac
Javier Sangrador <javier.sangrador@LDCXY309Y2F.local>
2024-11-21 01:44:55 +0100

    wip

--- src/test/java/com/javinsnc/microservices/orderservice/OrderServiceApplicationTests.java
+++ src/test/java/com/javinsnc/microservices/orderservice/OrderServiceApplicationTests.java
@@ -1,5 +1,6 @@
 package com.javinsnc.microservices.orderservice;
 
+import com.javinsnc.microservices.orderservice.stubs.InventoryClientStub;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.testcontainers.containers.MySQLContainer;
@@ -15,6 +17,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -40,11 +43,13 @@ class OrderServiceApplicationTests {
 				    "id": 1,
 				    "name": "bla",
 				    "orderNumber": 5,
-				    "skuCode": "someSkuCode",
+				    "skuCode": "iphone_15",
 				    "price": 10,
 				    "quantity": 1
 				}
 				""";
+
+		InventoryClientStub.stubInventoryCall("iphone_15", 10);
 
 		final var responseBodyString = RestAssured.given()
 				.contentType("application/json")
---
+++ src/test/java/com/javinsnc/microservices/orderservice/stubs/InventoryClientStub.java
@@ -0,0 +1,14 @@
+package com.javinsnc.microservices.orderservice.stubs;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class InventoryClientStub {
+
+    public static void stubInventoryCall(String skuCode, int quantity) {
+        stubFor(get(urlEqualTo("/api/inventory?skyCode=" + skuCode + "&quantity=" + quantity))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("true")));
+    }
+}
