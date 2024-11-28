dca406e4a4d2a32c184c9eeba6d42dc1c9b4987a
Javier Sangrador <javier.sangrador@LDCXY309Y2F.local>
2024-11-28 02:27:27 +0100

    wip

--- src/main/java/com/javinsnc/microservices/orderservice/config/RestClientConfig.java
+++ src/main/java/com/javinsnc/microservices/orderservice/config/RestClientConfig.java
@@ -2,11 +2,16 @@ package com.javinsnc.microservices.orderservice.config;
 
 import com.javinsnc.microservices.orderservice.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -17,10 +22,18 @@ public class RestClientConfig {
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
+                .requestFactory(getRequestFactory())
                 .baseUrl(inventoryServiceUrl)
                 .build();
         final var restClientAdapter = RestClientAdapter.create(restClient);
         final var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
